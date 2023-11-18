53d40177bfd8fff897496e20c1f93738bc5d8b4f
TCISERDAROGLU <okanserdaroglu2@gmail.com>
2023-11-18 14:32:33 +0300

    complete data and domain layer of getMovieList feature

--- feature/movie/domain/src/main/java/com/feature/movie/domain/di/DomainLayerModule.kt
+++ feature/movie/domain/src/main/java/com/feature/movie/domain/di/DomainLayerModule.kt
@@ -1,4 +1,18 @@
 package com.feature.movie.domain.di
 
+import com.feature.movie.domain.repository.MovieRepository
+import com.feature.movie.domain.use_cases.GetMovieList
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
 object DomainLayerModule {
+
+    @Provides
+    fun getMovieListUseCase(movieRepository: MovieRepository): GetMovieList =
+        GetMovieList(movieRepository)
+
 }
