1a71eff1a461f10fb4756a1bb69c9d7ff16dc4b7
TCISERDAROGLU <okanserdaroglu2@gmail.com>
2023-11-18 13:03:12 +0300

    complete network module

--- core/network/src/main/java/com/core/network/ApiService.kt
+++ core/network/src/main/java/com/core/network/ApiService.kt
@@ -4,6 +4,13 @@ import com.core.network.model.MovieListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+/**
+ * in network module
+ * 1- we create ApiService interface for retrofit
+ * 2- we create response model classes as DTO
+ * 3 - we create our dataProvider
+ */
+
 interface ApiService {
 
     // https://api.themoviedb.org/3/search/movie?api_key%3Capi_key%3Ef&query=Harry
---
+++ core/network/src/main/java/com/core/network/data_providers/MovieDataProviders.kt
@@ -0,0 +1,18 @@
+package com.core.network.data_providers
+
+import com.core.network.ApiService
+import javax.inject.Inject
+
+class MovieDataProviders
+@Inject constructor(
+    private val apiService: ApiService
+) {
+    suspend fun getMovieList(
+        apiKey: String,
+        q: String
+    ) = apiService.getMovieList(
+        apiKey = apiKey,
+        q = q
+    )
+
+}
